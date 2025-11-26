ad80ae4de916fc7866eda210024d2d89afcccbe9
VOpro <vopro@example.com>
2025-11-26 15:53:46 +0300

    Add POM structure for pages

---
+++ pages/locators.py
@@ -0,0 +1,6 @@
+class LoginPageLocators():
+    username_input = "[data-test='username']"
+    password_input = "[data-test='password']"
+    login_button = "[data-test='login-button']"
+
+    error_invalid_data = "[data-test='error']"
