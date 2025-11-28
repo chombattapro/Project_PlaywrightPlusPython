3fabb0276c07c093410c6d7af1b693bab806e46e
VOpro <vopro@example.com>
2025-11-28 14:56:21 +0300

    Changed class BasePage and LoginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,38 +1,29 @@
 from playwright.sync_api import expect
 from pages.base_page import BasePage
-from pages.locators import LoginPageLocators
+from configs.locators import LoginPageLocators
+from configs.testdata import LoginPageData
+from configs.urls import LoginPageResult
 
 class LoginPage(BasePage):
-    url = "https://saucedemo.com"
-    result_url = "https://www.saucedemo.com/inventory.html"
-
-    valid_username = "standard_user"
-    valid_password = "secret_sauce"
-
-    invalid_username = "p_user"
-    invalid_password = "p_sauce"
-
     def fill_valid_login_and_password(self):
-        self.page.locator(LoginPageLocators.username_input).fill(self.valid_username)
-        self.page.locator(LoginPageLocators.password_input).fill(self.valid_password)
+        self.page.locator(LoginPageLocators.username_input).fill(LoginPageData.valid_username)
+        self.page.locator(LoginPageLocators.password_input).fill(LoginPageData.valid_password)
+        self.page.locator(LoginPageLocators.login_button).click()
+        expect(self.page).to_have_url(LoginPageResult.result_url)
 
     def fill_invalid_login_and_password(self):
-        self.page.locator(LoginPageLocators.username_input).fill(self.invalid_username)
-        self.page.locator(LoginPageLocators.password_input).fill(self.invalid_password)
-
-    def click_button(self):
+        self.page.locator(LoginPageLocators.username_input).fill(LoginPageData.invalid_username)
+        self.page.locator(LoginPageLocators.password_input).fill(LoginPageData.invalid_password)
         self.page.locator(LoginPageLocators.login_button).click()
-
-    def check_result_url(self):
-        expect(self.page).to_have_url(self.result_url)
 
     def check_error_with_invalid_data(self):
         result = self.page.locator(LoginPageLocators.error_invalid_data)
-        expect(result).to_have_text("Epic sadface: Username and password do not match any user in this service")
+        expect(result).to_have_text(LoginPageData.error_invalid_data)
 
     def check_error_with_unfilled_fields(self):
+        self.page.locator(LoginPageLocators.login_button).click()
         result = self.page.locator(LoginPageLocators.error_invalid_data)
-        expect(result).to_have_text("Epic sadface: Username is required")
+        expect(result).to_have_text(LoginPageData.error_unfilled_fields)
 
 
 
