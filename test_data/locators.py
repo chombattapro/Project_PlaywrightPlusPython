e4604291aec943e40cb40442886be7facae638bc
VOpro <vopro@example.com>
2025-12-08 14:35:10 +0300

    Renamed locators

--- test_data/locators.py
+++ test_data/locators.py
@@ -3,12 +3,12 @@ class LoginPageLocators:
     password_input = "[data-test='password']"
     login_button = "[data-test='login-button']"
 
-    error_invalid_data = "[data-test='error']"
+    error_data = "[data-test='error']"
 
 class InventoryPageLocators:
-    product = "[data-test='inventory-item']"
-    product_title_first = "[data-test='item-4-title-link']"
-    product_price_first = "[data-test='inventory-item-price']"
-    product_btn_add_to_cart_first = "[data-test='add-to-cart-sauce-labs-backpack']"
+    product_card = "[data-test='inventory-item']"
+    product_title = ".inventory_item_name[data-test='inventory-item-name']"  # Заголовок
+    product_price = ".inventory_item_price[data-test='inventory-item-price']"  # Цена
+    add_to_cart_button = "button[data-test^='add-to-cart-']"
 
     cart_button = "[data-test='shopping-cart-link']"
