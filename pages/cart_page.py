cfb16d5f082bb7d1b340396b0a3cc58d691a8fc1
VOpro <vopro@example.com>
2025-12-12 18:22:48 +0300

    Added __init__ , @properties, and new product quantity checks. Moved the faker to a global variable

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,36 +1,50 @@
 from playwright.sync_api import expect
-from pages.base_page import BasePage
+from pages.base_page import BasePage, BASE_URL
 from test_data.locators import CartPageLocators
+from faker import Faker
+
+fake = Faker()
 
 class CartPage(BasePage):
-    url = f"{BasePage.base_url}/cart.html"
+    url = f"{BASE_URL}/cart.html"
 
-    def verify_checkout_button(self):
-        checkout_button = self.page.locator(CartPageLocators.checkout_button)
-        expect(checkout_button).to_be_visible()
-        expect(checkout_button).to_be_enabled()
-        checkout_button.click()
+    def __init__(self, page):
+        super().__init__(page)
+        self.locators = CartPageLocators
 
-    def fill_in_user_details(self, faker):
-        self.page.locator(CartPageLocators.first_name).fill(faker.first_name())
-        self.page.locator(CartPageLocators.last_name).fill(faker.last_name())
-        self.page.locator(CartPageLocators.postal_code).fill(faker.postcode())
-        continue_button = self.page.locator(CartPageLocators.continue_button)
-        expect(continue_button).to_be_visible()
-        expect(continue_button).to_be_enabled()
-        continue_button.click()
+    @property
+    def first_name(self):
+        return self.page.get_by_test_id(self.locators.first_name)
 
-    def verify_finish_button(self):
-        finish_button = self.page.locator(CartPageLocators.finish_button)
-        expect(finish_button).to_be_visible()
-        expect(finish_button).to_be_enabled()
-        finish_button.click()
+    @property
+    def last_name(self):
+        return self.page.get_by_test_id(self.locators.last_name)
 
-    def check_massage_about_a_successful_purchase(self):
-        successful_massage = self.page.locator(CartPageLocators.successful_purchase_message)
+    @property
+    def postal_code(self):
+        return self.page.get_by_test_id(self.locators.postal_code)
+
+    @property
+    def product_card(self):
+        return self.page.get_by_test_id(self.locators.product_card_in_cart)
+
+    def verify_products_count_in_cart(self, expected_count):
+        product_card_counter = self.product_card
+        expect(product_card_counter).to_have_count(expected_count)
+        self.page.get_by_test_id(self.locators.checkout_button).click()
+
+    def fill_in_user_details(self):
+        self.first_name.fill(fake.first_name())
+        self.last_name.fill(fake.last_name())
+        self.postal_code.fill(fake.postcode())
+        self.page.get_by_test_id(self.locators.continue_button).click()
+
+    def verify_products_count_in_final_checkout(self, expected_count):
+        product_card_counter = self.product_card
+        expect(product_card_counter).to_have_count(expected_count)
+        self.page.get_by_test_id(self.locators.finish_button).click()
+
+    def verify_message_about_a_successful_purchase(self):
+        successful_massage = self.page.get_by_test_id(self.locators.successful_purchase_message)
         expect(successful_massage).to_be_visible()
         expect(successful_massage).to_contain_text("Thank you for your order!")
-        back_home_button = self.page.locator(CartPageLocators.back_home_button)
-        expect(back_home_button).to_be_visible()
-        expect(back_home_button).to_be_enabled()
-        back_home_button.click()
