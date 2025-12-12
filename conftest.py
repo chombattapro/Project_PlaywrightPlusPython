4d7c53e0f3cf71d63174d3f0aebb1198847ec0ee
VOpro <vopro@example.com>
2025-12-12 18:10:27 +0300

    Changed locators search to data-test

--- conftest.py
+++ conftest.py
@@ -3,6 +3,10 @@ from playwright.sync_api import Page
 from pages.login_page import LoginPage
 from faker import Faker
 
+@pytest.fixture(scope="session", autouse=True)
+def configure_test_selectors(playwright):
+    playwright.selectors.set_test_id_attribute("data-test")
+
 @pytest.fixture(scope="function")
 def faker():
     return Faker()
