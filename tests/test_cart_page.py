fd025a8d2c4536e3dc2b92047ff4e810aafff0ac
VOpro <vopro@example.com>
2025-12-12 18:38:57 +0300

    Modified tests based on new changes to the POM

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -2,20 +2,14 @@ from playwright.sync_api import Page
 from pages.inventory_page import InventoryPage
 from pages.cart_page import CartPage
 
-def test_the_full_cycle_of_successful_purchase(login, page: Page, faker):
+def test_verify_successful_purchase(login, page: Page):
     inventory_page = InventoryPage(login)
-    inventory_page.add_to_cart(0)
-    inventory_page.add_to_cart(1)
-    inventory_page.add_to_cart(2)
-    inventory_page.add_to_cart(3)
-    inventory_page.verify_cart_button()
+    products = [0, 1, 2, 3]
+    for index in products:
+        inventory_page.add_to_cart(index)
+    inventory_page.click_cart_button()
     cart_page = CartPage(page)
-    cart_page.verify_checkout_button()
-    cart_page.fill_in_user_details(faker)
-    cart_page.verify_finish_button()
-    cart_page.check_massage_about_a_successful_purchase()
-
-
-
-
-
+    cart_page.verify_products_count_in_cart(len(products))
+    cart_page.fill_in_user_details()
+    cart_page.verify_products_count_in_final_checkout(len(products))
+    cart_page.verify_message_about_a_successful_purchase()
