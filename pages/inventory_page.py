5ae8374a326896fa702b6df128b29374cf964726
VOpro <vopro@example.com>
2025-12-16 13:35:59 +0300

    Initialization of locators has been moved to the constructor. Tests modified

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -8,32 +8,28 @@ class InventoryPage(BasePage):
     def __init__(self, page):
         super().__init__(page)
         self.locators = InventoryPageLocators
-
-    @property
-    def product_card(self):
-        return self.page.get_by_test_id(self.locators.product_card)
+        self.product_card = self.page.get_by_test_id(self.locators.product_card)
+        self.product_title = self.page.get_by_test_id(self.locators.product_title)
+        self.product_price = self.page.get_by_test_id(self.locators.product_price)
+        self.add_to_cart_button = self.page.locator(self.locators.add_to_cart_button)
+        self.cart_button = self.page.get_by_test_id(self.locators.cart_button)
 
     def verify_products_count(self, expected_count):
-        product_card_counter = self.product_card
-        expect(product_card_counter).to_have_count(expected_count)
+        expect(self.product_card).to_have_count(expected_count)
 
     def verify_product_card(self, card_index):
         """
         Берем конкретную карточку по индексу и проверяем элементы внутри этой карточки
         """
         product_card = self.product_card.nth(card_index)
-        title = product_card.locator(self.locators.product_title)
-        expect(title).to_be_visible()
-        expect(title).to_be_enabled()
-        price = product_card.locator(self.locators.product_price)
-        expect(price).to_be_visible()
-        add_to_cart_button = product_card.locator(self.locators.add_to_cart_button)
-        expect(add_to_cart_button).to_be_visible()
-        expect(add_to_cart_button).to_be_enabled()
+        expect(product_card.locator(self.product_title)).to_be_visible()
+        expect(product_card.locator(self.product_title)).to_be_enabled()
+        expect(product_card.locator(self.product_price)).to_be_visible()
+        expect(product_card.locator(self.add_to_cart_button)).to_be_visible()
+        expect(product_card.locator(self.add_to_cart_button)).to_be_enabled()
 
     def click_cart_button(self):
-        self.page.get_by_test_id(self.locators.cart_button).click()
+        self.cart_button.click()
 
     def add_to_cart(self, card_index):
-        product_card = self.product_card.nth(card_index)
-        product_card.locator(self.locators.add_to_cart_button).click()
+        self.product_card.nth(card_index).locator(self.add_to_cart_button).click()
